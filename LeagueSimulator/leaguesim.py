fc79c0511134d88451f7718d90cf60d90c941495
adt9595 <58305256+adt9595@users.noreply.github.com>
2020-01-03 16:56:33 +0000

    Finished and integrated player attributes
    Incorporated player attributes into formula for the probability of scoring a goal, dictated by regularisation parameter 'teamDeltaFactor'
    
    Increasing this parameter from 0 shows a positive correlation in the highest points total for a given season

--- LeagueSimulator/leaguesim.py
+++ LeagueSimulator/leaguesim.py
@@ -16,6 +16,7 @@ import math
 import sys
 import random
 import time
+import matplotlib.pyplot as plt
 from itertools import permutations
 
 #pygame.init()
@@ -57,6 +58,14 @@ def stddev(x):
 def listProduct(x,y):
     return sum(i[0]*i[1] for i in zip(x,y))
 
+def plotFigure(data,stepsize):
+    steps = [stepsize*i for i in range(25)]
+    plt.plot(steps,data)
+    plt.plot((0,stepsize*25),(87.074,87.074))
+    plt.axis([0, stepsize*25, min(data), max(data)])
+    plt.show()
+
+
 #####################################################
 
 #class Game:
@@ -86,11 +95,12 @@ class League:
     teams - Array, list of teams
     numIterations - Int, number of iterations to run
     """
-    def __init__(self,name,teams,numIterations):
+    def __init__(self,name,teams,teamDeltaFactor,numIterations):
         self.name = name
         self.teams = teams
         self.numTeams = len(teams)
         self.gamesPlayed = 0    
+        self.teamDeltaFactor = teamDeltaFactor
         self.schedule = list(permutations([i for i in range(self.numTeams)],2))
         self.numIterations = numIterations
         if numIterations == 1:
@@ -104,34 +114,34 @@ class League:
         self.gamesPlayed = 0
         for team in self.teams:
             team.reset()
-        
+    
+    
     def getTable(self):
         self.sortTeams()
         if(self.verbose):
             table = "{0:3}|{1:2}|{2:2}|{3:2}|{4:2}|{5:2}|{6:2}|{7:2}|{8:2}"
-            print("League Table:")
+            print(self.name + " Table:")
             print(table.format("Pos ","Pl","W","D","L","GF","GA","GD","Pts"))
             for i in range(len(self.teams)):
                 tm = self.teams[i]
-                results = tm.getResults()
-                goals = tm.getGoals()
+                results = tm.results
+                goals = tm.goals
                 row = (str(i+1),str(self.gamesPlayed),str(results[0]),str(results[1]),str(results[2]),
-                       str(goals[0]),str(goals[1]),str(goals[0]-goals[1]),str(tm.getPoints()))
+                       str(goals[0]),str(goals[1]),str(goals[0]-goals[1]),str(tm.points))
                 if i == 4 or i == 17: print("-----------------------------")
-                print(tm.getName(),table.format(*row))
+                print(tm.name,table.format(*row))
         
 
             
     def playMatch(self,team1,team2):
-        Match(team1,team2,False)
-        #self.getTable()
+        Match(team1,team2,self.teamDeltaFactor,False)
     
     def sortTeams(self):
-        self.teams = sorted(self.teams,key=lambda x: x.getPoints(), reverse = True)
+        self.teams = sorted(self.teams,key=lambda x: x.points, reverse = True)
         
     def leagueLoop(self):
-        avg = 0
-        dev = 0
+        self.avg = 0
+        self.dev = 0
         mostPoints = 0
         winningPoints = []
         
@@ -145,18 +155,30 @@ class League:
                     self.gamesPlayed += 1
             self.gamesPlayed = 2 * self.numTeams - 2 
             self.getTable()
-            winner = self.teams[0].getPoints()
+            
+            winner = self.teams[0].points
             winningPoints.append(winner)
+            winStreak = max([team.topWinStreak for team in teams])
+            unbeatenStreak = max([team.topUnbeatenStreak for team in teams])
+            loseStreak = max([team.topLoseStreak for team in teams])
+            if self.verbose:
+                print("Longest win streak: " + str(winStreak))
+                print("Longest unbeaten streak: " + str(unbeatenStreak))
+                print("Longest lose streak: " + str(loseStreak))
+            
             if winner > mostPoints:
                 mostPoints = winner
+                
             self.reset()
+            
         if not self.verbose:
-            print(winningPoints)
-            avg = mean(winningPoints)
-            dev = stddev(winningPoints)
-            print("Avg:",avg)
-            print("Std Dev:",dev)
-            print("Most points:",mostPoints)
+            #print(winningPoints)
+            self.avg = mean(winningPoints)
+            self.dev = stddev(winningPoints)
+            #print("Avg:",self.avg)
+            #print("Std Dev:",self.dev)
+            #print("Most points:",mostPoints)
+            print("Iteration Complete")
     
 class Team:
     """
@@ -171,7 +193,10 @@ class Team:
         
         self.reset()
         self.loadData(path)
+                
         self.setGoalProbabilities()
+        self.attack = sum([player.attributes[2] for player in self.players])
+        self.defence = sum([player.attributes[0] for player in self.players])
         
     def setGoalProbabilities(self):
         self.goalProbabilities = [player.goalProbability for player in self.players]
@@ -183,59 +208,104 @@ class Team:
         self.goals = [0,0]
         self.results = [0,0,0]
         self.goalDifference = 0
+        self.topScorer = None
+        self.topWinStreak = 0
+        self.topUnbeatenStreak = 0
+        self.topLoseStreak = 0
+        self.unbeatenStreak = 0
+        self.winStreak = 0
+        self.loseStreak = 0
+        self.formLimitUpper = 55
+        self.formLimitLower = 30
+        
+        
         self.setForm()
     
-    def getName(self):
-        return self.name
-    
-    def getForm(self):
-        return self.form
-    
-    def getGoals(self):
-        return self.goals
-    
-    def getPlayers(self):
-        return self.players
-    
-    def getPoints(self):
-        return self.points
-    
-    def getResults(self):
-        return self.results
     
     def setName(self,name):
         self.name = name
         
     def setForm(self):
-        self.form = 30 + int(round((weightedSum(self.previousFinishes,0.5) / weightedSum([20,20,20,20,20],0.5))*70))
+        baseForm = 35
+        prevForm = int(round((weightedSum(self.previousFinishes,0.5) / weightedSum([20,20,20,20,20],0.5))))
+        previousFinishFactor = 18
+        
+        self.form = baseForm + previousFinishFactor*prevForm
     
     def setResult(self,index):
+        # win
         if index == 0:
+            self.winStreak += 1
+            self.unbeatenStreak += 1
+            if self.winStreak > self.topWinStreak:
+                self.topWinStreak = self.winStreak
+            if self.unbeatenStreak > self.topUnbeatenStreak:
+                self.topUnbeatenStreak = self.unbeatenStreak
+                
             self.results[0] += 1
             self.points += 3
-            if self.form > 35:
-                self.form -= random.randint(1,4)
+            if self.form > self.formLimitLower:
+                self.form -= random.randint(1,3)
+        # draw
         elif index == 1:
+            self.winStreak = 0
+            self.unbeatenStreak += 1
+            if self.unbeatenStreak > self.topUnbeatenStreak:
+                self.topUnbeatenStreak = self.unbeatenStreak
+                
             self.results[1] += 1
             self.points += 1
-            if self.form < 35: 
+            if self.form < self.formLimitLower: 
                 self.form -= random.randint(-2,1)
-            elif self.form >= 35 and self.form < 50 :
+            elif self.form >= self.formLimitLower and self.form < 50 :
                 self.form -= random.randint(-1,2)
-            elif self.form >= 50 and self.form < 65 :
-                self.form -= random.randint(0,4)
-            elif self.form >= 65:
+            elif self.form >= 50 and self.form < self.formLimitUpper :
                 self.form -= random.randint(0,3)
+            elif self.form >= self.formLimitUpper:
+                self.form -= random.randint(0,3)
+        # loss
         elif index == 2:
+            self.winStreak = 0
+            self.unbeatenStreak = 0
+            self.loseStreak += 1
+            if self.loseStreak > self.topLoseStreak:
+                self.topLoseStreak = self.loseStreak
             self.results[2] += 1
-            if self.form < 65:
-                self.form += random.randint(1,4)
-    
+            if self.form < self.formLimitUpper:
+                self.form += random.randint(1,3)
+        self.changePlayerStats(index)
+        self.topScorer = max([player.goals for player in self.players])
+        self.attack
+        
     def increaseGoals(self,index):
         if(index < len(self.goals)):
             self.goals[index] += 1
             self.goalDifference = self.goals[0] - self.goals[1]
     
+    def changePlayerStats(self,index):
+        # win
+        if index == 0:
+            for i in range(3):
+                playerIndex = random.randint(0,10)
+                statIndex = random.randint(0,2)
+                tst = self.players[playerIndex]
+                tst.attributes[statIndex] += 1
+        # draw
+        elif index == 1:
+            playerIndex = random.randint(0,10)
+            statIndex = random.randint(0,2)
+            self.players[playerIndex].attributes[statIndex] += 1
+            playerIndex = random.randint(0,10)
+            statIndex = random.randint(0,2)
+            self.players[playerIndex].attributes[statIndex] -= 1
+        # loss
+        elif index == 2:
+            for i in range(3):
+                playerIndex = random.randint(0,10)
+                statIndex = random.randint(0,2)
+                self.players[playerIndex].attributes[statIndex] -= 1
+                
+        
     def loadData(self,path):
         # Data format:
         # TeamName
@@ -288,22 +358,17 @@ class Player:
     
     def formatAttributes(self,attributes):
         attributes = attributes.split(' ')
+        #adjustedAttributes = []
         for i in range(len(attributes)):
-            attributes[i] = int(attributes[i])
+            attributes[i] = float(attributes[i]) * positionMultipliers[self.position][i]
         return attributes
     
     def setRating(self):
-        multiplier = positionMultipliers[self.position] 
-        self.overallRating = listProduct(multiplier,self.attributes)        
+        self.overallRating = sum(self.attributes)     
     
     def scoreGoal(self):
         self.goals += 1
     
-    def getName(self):
-        return self.name
-    
-    def getPosition(self):
-        return self.position
         
 
         
@@ -313,14 +378,15 @@ class Match:
     team1 - Team, home team
     team2 - Team, away team
     """
-    def __init__(self,team1,team2,verbose):
+    def __init__(self,team1,team2,teamDeltaFactor,verbose):
         self.team1 = team1
         self.team2 = team2
         self.verbose = verbose
         self.goals = [0,0]
         self.minuteCounter = 0
-        self.goalChance1 = self.team1.getForm()
-        self.goalChance2 = self.team2.getForm()
+        self.teamDeltaFactor = teamDeltaFactor
+        self.goalChance1 = int(round(self.team1.form - self.teamDeltaFactor*(team1.attack - team2.defence)))
+        self.goalChance2 = int(round(self.team2.form - self.teamDeltaFactor*(team2.attack - team1.defence)))
         if self.verbose:
             print(self.team1.goalProbabilities)
         self.matchLoop()
@@ -331,29 +397,27 @@ class Match:
         self.team2.increaseGoals(1 - index)
         if index == 0:
             playerIndex = np.random.choice(list(range(11)),1,self.team1.goalProbabilities)[0]
+            self.team1.players[playerIndex].scoreGoal()
             if self.verbose:
-                print("GOAL! " + self.team1.getName() + "" + self.team1.players[playerIndex].getName())
+                print("GOAL! " + self.team1.name + "" + self.team1.players[playerIndex].name)
         elif index == 1:
             playerIndex = np.random.choice(list(range(11)),1,self.team2.goalProbabilities)[0]
+            self.team2.players[playerIndex].scoreGoal()
             if self.verbose:
-                print("GOAL! " + self.team2.getName() + "" + self.team2.players[playerIndex].getName())
+                print("GOAL! " + self.team2.name + "" + self.team2.players[playerIndex].name)
         if self.verbose:
             print("")
             time.sleep(1.5)
-                
-        
-        
     
     def matchLoop(self):
         while self.minuteCounter <= 90:
-            
             if random.randint(0,self.goalChance1) == 0:
                 self.scoreGoal(0)
             if random.randint(0,self.goalChance2) == 1:
                 self.scoreGoal(1)
             if self.verbose:    
-                print(str(self.minuteCounter) + ': ' + self.team1.getName() + " " 
-                      + str(self.goals[0]) + ' - '  + str(self.goals[1]) + " " + self.team2.getName())
+                print(str(self.minuteCounter) + ': ' + self.team1.name + " " 
+                      + str(self.goals[0]) + ' - '  + str(self.goals[1]) + " " + self.team2.name)
                 time.sleep(0.08)
             self.minuteCounter += 1
         
@@ -374,8 +438,8 @@ positionMultipliers = {
     "CB": [1.7,1.0,0.3],
     "CDM": [1.4,1.1,0.6],
     "CM": [0.6,1.7,0.7],
-    "RM": [0.5,1.3,1.2],
-    "LM": [0.5,1.3,1.2],
+    "RM": [0.5,1.4,1.2],
+    "LM": [0.5,1.4,1.2],
     "RW" : [0.3,1.2,1.5],
     "LW" : [0.3,1.2,1.5],
     "CAM" : [0.3,1.4,1.3],
@@ -383,11 +447,21 @@ positionMultipliers = {
 }
 
 teams = []
+averages = []
+stepsize = 0.004
+
+# Populate league
 for i in range(20):
     teams.append(Team('teamdata/team{0}.txt'.format(i)))
 
-prem = League('Premier League',teams,numIterations=40)
+# Run multiple leagues with varying team rating parameters
+for i in range(25):
+    prem = League('Premier League',teams,0.0 + (stepsize * i),numIterations=100)
+    averages.append(prem.avg)
+plotFigure(averages,stepsize)
+    
 #testgame = Match(teams[0],teams[1],verbose=True)
+
 
 """
 REAL STATS:
